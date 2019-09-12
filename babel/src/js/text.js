91e105b56358a2b3bbec4498df8cb8cff91ddc74
Moshe Shabes <moshnaf@gmail.com>
2019-09-11 22:07:53 -0500

    first commit

---
+++ babel/src/js/text.js
@@ -0,0 +1,7 @@
+function text () {
+    document.body.innerHTML = '<p>Hola mundo! desde Webpack! index</p>'
+}
+
+
+
+export default text
---
+++ multiple-entry-points/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/js/index.js'),
+        precios: path.resolve(__dirname, 'src/js/precios.js'),
+        contacto: path.resolve(__dirname, 'src/js/contacto.js'),
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+}
+
+
+
